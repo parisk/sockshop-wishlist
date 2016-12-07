9e0d04a3ac6b46e757275b5622d1917138dc1a85
Paris Kasidiaris <paris@sourcelair.com>
2016-12-07 13:56:46 +0200

    Namespace wishlist items

--- wishlist/wishlists/serializers.py
+++ wishlist/wishlists/serializers.py
@@ -13,3 +13,17 @@ class WishlistItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = WishlistItem
         fields = ('id', 'wishlist', 'product')
+
+class NamespacedWishlistItemSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = WishlistItem
+        fields = ('id', 'product',)
+
+    def create(self, validated_data, wishlist):
+        wishlist.refresh_from_db()
+        wishlist_item = WishlistItem.objects.create(
+            wishlist=wishlist, **validated_data
+        )
+
+        return wishlist_item
--- wishlist/wishlists/views.py
+++ wishlist/wishlists/views.py
@@ -1,4 +1,6 @@
+from rest_framework import status
 from rest_framework import viewsets
+from rest_framework.response import Response
 
 from wishlists import models
 from wishlists import serializers
@@ -10,8 +12,27 @@ class WishlistViewSet(viewsets.ModelViewSet):
 
 
 class WishlistItemViewSet(viewsets.ModelViewSet):
-    serializer_class = serializers.WishlistItemSerializer
+    serializer_class = serializers.NamespacedWishlistItemSerializer
 
     def get_queryset(self):
         wishlist = models.Wishlist.objects.get(id=self.kwargs['wishlist_id'])
         return models.WishlistItem.objects.filter(wishlist=wishlist)
+
+    def create(self, request, wishlist_id):
+        wishlist = models.Wishlist.objects.get(id=self.kwargs['wishlist_id'])
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        wishlist_item = serializer.create(serializer.data, wishlist)
+        headers = self.get_success_headers(serializer.data)
+
+        response_serializer = serializers.WishlistItemSerializer(
+            wishlist_item, context={
+                'request': request
+            }
+        )
+        response = Response(
+            response_serializer.data,
+            status=status.HTTP_201_CREATED,
+            headers=headers
+        )
+        return response
