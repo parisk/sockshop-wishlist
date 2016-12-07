06ef8c5014f15d20d32b877bda8a241850b0b99c
Paris Kasidiaris <paris@sourcelair.com>
2016-12-07 13:01:23 +0200

    Restructure

--- wishlist/wishlist/settings.py
+++ wishlist/wishlist/settings.py
@@ -42,7 +42,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'items'
+    'customers',
+    'products',
+    'wishlists',
 ]
 
 MIDDLEWARE = [
