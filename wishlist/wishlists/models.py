d558f7266b5b839a8673b4ee406b3219f7d605ff
Paris Kasidiaris <paris@sourcelair.com>
2016-12-07 13:06:36 +0200

    Create models

--- wishlist/wishlists/models.py
+++ wishlist/wishlists/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+from customers.models import Customer
+from products.models import Product
+
+
+class Wishlist(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+
+
+class WishlistItem(models.Model):
+    wishlist = models.ForeignKey(Wishlist, on_delete=models.CASCADE)
+    products = models.ForeignKey(Product, on_delete=models.CASCADE)
